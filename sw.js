f53b1ceaac3c13b0f6ca24f9d660aa09f3da372e
enriPD13 <enrico.albuzio@gmail.com>
2026-07-21 12:29:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Registro Spese — service worker: app shell in cache, API sempre in rete */
-const CACHE = "registro-spese-v7.3";
+const CACHE = "registro-spese-v7.4";
 const SHELL = [
   "./",
   "./index.html",
